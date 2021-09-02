57d281ee4d1af7c7eb9e9d50afcad0788f85edfd
Mehmet <tekmanmehmetyasar@protonmail.com>
2021-09-02 12:27:37 +0300

    Javascript script which sends a get request to the model api, gets the result and displays the result was written.

---
+++ app/static/scripts/sendModel.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+
+
+    let button = document.getElementById("send-button");
+
+
+
+    button.onclick = function () {
+
+        console.log("Button clicked!!!!");
+        let jsonData = JSON.stringify({
+            answer: document.getElementById("answer-area").value
+        })
+
+        console.log(jsonData)
+
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application / json',
+                'Content-Type': 'application/json'
+            },
+            body: jsonData
+        };
+        fetch('https://degerlendirenzeka-deploy.herokuapp.com/api/model/predict', options)
+            .then(response => response.json())
+            .then(response => {
+                document.getElementById("rcontainer-result-text").innerText = "Değerlendiren Zeka'nın tahminine göre bu cevap"
+                document.getElementById("rcontainer-result-percentage").innerText = ` %${response.result} doğru`;
+                
+            });
+
+
+    }
+
+
+}
