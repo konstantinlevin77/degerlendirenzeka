65426b0ac8886d0fcc4c7c38ff3d02cb2ec4fa39
Mehmet <tekmanmehmetyasar@protonmail.com>
2021-09-02 12:12:11 +0300

    Database entities Answer (for the table 'answers') and Question (for the table 'questions' were created.

---
+++ app/Answer.py
@@ -0,0 +1,6 @@
+class Answer:
+
+    def __init__(self,id=None,question_id=None,answer=None):
+        self.id = id
+        self.question_id = question_id
+        self.answer = answer
---
+++ app/Question.py
@@ -0,0 +1,6 @@
+class Question:
+    def __init__(self,id=None,question_text=None):
+        self.id = id
+        self.question_text = question_text
+
+    
